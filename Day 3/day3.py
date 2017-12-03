cbf109deff30ca7ce05d602f04e49f991baf2985
Sebastian Osiński <seb.osinski@gmail.com>
2017-12-03 12:46:33 +0100

    Add solution for part 1 of Day 3

---
+++ Day 3/day3.py
@@ -0,0 +1,62 @@
+from enum import Enum
+
+class Direction(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+input = 312051
+
+# Part 1
+
+def find_square_position(square):
+    x = 0
+    y = 0
+    i = 1
+
+    direction = Direction.DOWN
+    steps_to_make = 0
+    steps_made_in_current_direction = 0
+
+    while i < square:
+        if direction == Direction.RIGHT:
+            if steps_made_in_current_direction < steps_to_make:
+                x += 1
+                steps_made_in_current_direction += 1
+                i += 1
+            else:
+                direction = Direction.UP
+                steps_made_in_current_direction = 0
+        elif direction == Direction.UP:
+            if steps_made_in_current_direction < steps_to_make:
+                y += 1
+                steps_made_in_current_direction += 1
+                i += 1
+            else:
+                direction = Direction.LEFT
+                steps_to_make += 1
+                steps_made_in_current_direction = 0
+        elif direction == Direction.LEFT:
+            if steps_made_in_current_direction < steps_to_make:
+                x -= 1
+                steps_made_in_current_direction += 1
+                i += 1
+            else:
+                direction = Direction.DOWN
+                steps_made_in_current_direction = 0
+        else:
+            if steps_made_in_current_direction < steps_to_make:
+                y -= 1
+                steps_made_in_current_direction += 1
+                i += 1
+            else:
+                direction = Direction.RIGHT
+                steps_to_make += 1
+                steps_made_in_current_direction = 0
+    return (x, y)
+
+def manhatten_dist(point):
+    return abs(point[0]) + abs(point[1])
+
+print(manhatten_dist(find_square_position(input)))
