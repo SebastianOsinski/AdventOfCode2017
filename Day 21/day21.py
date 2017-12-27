c9a9f6d275b03207631bd07e619a2c08848af553
Sebastian Osiński <seb.osinski@gmail.com>
2017-12-27 15:49:23 +0100

    Add solution for Day 21

--- Day 21/day21.py
+++ Day 21/day21.py
@@ -28,5 +28,90 @@ def rotate_right(matrix):
 
     return rotated_matrix
 
+def flip(matrix):
+    flipped_matrix = []
 
-print(rotate_right([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    size = len(matrix)
+
+    for i in range(size):
+        flipped_matrix.append(matrix[i][::-1])
+
+    return flipped_matrix
+
+def empty_grid(size):
+    grid = []
+    for _ in range(size):
+        grid.append(['.'] * size)
+    return grid
+
+def submatrix(matrix, top_left_corner_x, top_left_corner_y, size):
+    submatrix = empty_grid(size)
+
+    for i in range(size):
+        for j in range(size):
+            submatrix[i][j] = matrix[top_left_corner_x + i][top_left_corner_y + j]
+
+    return submatrix
+
+def enhanced(matrix):
+    input = matrix
+    for _ in range(4):
+        key = str(input)
+        if key in RULES:
+            return RULES[key]
+
+        flipped_input = flip(input)
+        key = str(flipped_input)
+
+        if key in RULES:
+            return RULES[key]
+        input = rotate_right(input)
+    raise Exception("No rule for given input " + str(input))
+
+def replace_in_matrix(matrix, submatrix, top_left_corner_x, top_left_corner_y):
+    size = len(submatrix)
+    for i in range(size):
+        for j in range(size):
+            matrix[top_left_corner_x + i][top_left_corner_y + j] = submatrix[i][j]
+
+def enhance(grid):
+    size = len(grid)
+
+    if size % 2 == 0:
+        times = len(grid) // 2
+        new_size = times * 3
+        step = 2
+        new_step = 3
+    else:
+        times = len(grid) // 3
+        new_size = times * 4
+        step = 3
+        new_step = 4
+
+    new_grid = empty_grid(new_size)
+
+    for i in range(times):
+        for j in range(times):
+            subgrid = submatrix(grid, i * step, j * step, step)
+            enhanced_subgrid = enhanced(subgrid)
+            replace_in_matrix(new_grid, enhanced_subgrid, i * new_step, j * new_step)
+
+    return new_grid
+
+def count_on_pixels(grid):
+    size = len(grid)
+    count = 0
+    for i in range(size):
+        for j in range(size):
+            if grid[i][j] is '#':
+                count += 1
+
+    return count
+
+grid = [['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']]
+iterations = 18
+
+for i in range(iterations):
+    grid = enhance(grid)
+
+print(count_on_pixels(grid))
